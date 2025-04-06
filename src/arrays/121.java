a82659d1236e80b1fb74b28a07e4fcaf5ecffac7
FaizanahmedSy <saiyed.faizanahmed1@gmail.com>
2025-04-06 15:26:16 +0530

    add solution for Best Time to Buy and Sell Stock add solution for Contains Duplicate

---
+++ src/arrays/121.java
@@ -0,0 +1,35 @@
+// 121. Best Time to Buy and Sell Stock
+// Solved
+// Easy
+// Topics
+// Companies
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;  // found a better day to buy
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;  // better profit if we sell today
+            }
+        }
+
+        return maxProfit;
+    }
+}
