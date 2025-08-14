0221c850d4d9ad2b4110528ca0f5083b5510bd36
FaizanahmedSy <saiyed.faizanahmed1@gmail.com>
2025-08-14 08:51:34 +0530

    feat: rename functions for clarity and add console log for app2lengthOfLastWord

--- javascript/string/58-length-of-last-word.js
+++ javascript/string/58-length-of-last-word.js
@@ -4,11 +4,47 @@
  */
 
 // Approach 1 - Builtin methods
-var lengthOfLastWord = function (s) {
+var app1lengthOfLastWord = function (s) {
   s = s.trim();
   s = s.split(" ");
 
   return s[s.length - 1].length;
 };
 
-console.log(lengthOfLastWord("  hello my   name   is   FAIZAN          "));
+var app2lengthOfLastWord = function (s) {
+  s = s.trim();
+  s = s.split(" ");
+
+  return s[s.length - 1].length;
+};
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var app2lengthOfLastWord = function (s) {
+  // start from end
+  let n = s.length - 1;
+
+  while (n >= 0) {
+    if (s[n] === " ") {
+      --n;
+    } else {
+      break;
+    }
+  }
+
+  let count = 0;
+  while (n >= 0) {
+    if (s[n] != " ") {
+      --n;
+      ++count;
+    } else {
+      break;
+    }
+  }
+
+  return count;
+};
+
+console.log(app2lengthOfLastWord("  hello my   name   is   FAIZAN          "));
