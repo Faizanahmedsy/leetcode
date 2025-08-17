54d4886c692330b2b0cc2c1a197fe0ea2ff99010
FaizanahmedSy <saiyed.faizanahmed1@gmail.com>
2025-08-17 14:50:37 +0530

    feat: rename hasCycle function to App1hasCycle and add App2hasCycle implementation

--- javascript/linkedlist/141-linked-list-cycle.js
+++ javascript/linkedlist/141-linked-list-cycle.js
@@ -10,7 +10,7 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var hasCycle = function (head) {
+var App1hasCycle = function (head) {
   const set = new Set();
   let current = head;
   while (current != null) {
@@ -22,4 +22,19 @@ var hasCycle = function (head) {
   }
 
   return false;
+};
+
+var App2hasCycle = function (head) {
+  if (!head) return false;
+
+  let tortoise = head; // 🐢 1 step
+  let hare = head.next; // 🐇 2 steps
+
+  while (tortoise !== hare) {
+    if (!hare || !hare.next) return false; // reached end
+    tortoise = tortoise.next;
+    hare = hare.next.next;
+  }
+
+  return true; // cycle found
 };
