995974b196b9e2760ff5768cac8e6492f8721bbb
FaizanahmedSy <saiyed.faizanahmed1@gmail.com>
2025-08-31 20:40:18 +0530

    add impovement in bubble sort

--- javascript/sorting/bubble-sort.js
+++ javascript/sorting/bubble-sort.js
@@ -11,4 +11,22 @@ function bubbleSort(arr) {
   return arr;
 }
 
+function bubbleSort2(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let isSwapper = false;
+
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+
+        isSwapper = true;
+      }
+    }
+    if (!isSwapper) break;
+  }
+  return arr;
+}
+
 console.log(bubbleSort([124, 23, 35, 74, 32]));
