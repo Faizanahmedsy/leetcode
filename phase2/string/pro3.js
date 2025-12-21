850dc85177aa6fb7f05d3accd2a3734b0c794753
FaizanahmedSy <saiyed.faizanahmed1@gmail.com>
2025-12-21 16:00:29 +0530

    feat: Replace cat with dog

--- phase2/string/pro3.js
+++ phase2/string/pro3.js
@@ -47,18 +47,83 @@ function task3(str = "apple") {
 }
 
 // Replace all lowercase letters with "*"
-function task3(str = "aBcdE") {}
+function task4(str = "aBcdE") {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (char >= "a" && char <= "z") {
+      result += "*";
+    } else {
+      result += char;
+    }
+  }
+
+  return result;
+}
 
 // Replace every 'a' with 'z'
+function task5(str = "banana") {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (char == "a") {
+      result += "z";
+    } else {
+      result += char;
+    }
+  }
+
+  return result;
+}
 
 // Replace digits with '#'
 
+function task6(str = "a1b2c3") {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (Number.isFinite(Number(char))) {
+      result += "#";
+    } else {
+      result += char;
+    }
+  }
+
+  return result;
+}
+
+console.log(task6());
+
 // Replace "cat" with "dog"
+
+function task7(str = "catapuit") {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (
+      str[i].toLowerCase() == "c" &&
+      str[i + 1].toLowerCase() == "a" &&
+      str[i + 2].toLowerCase() == "t"
+    ) {
+      result += "dog";
+      i + 2;
+      continue;
+    } else {
+      result += char;
+    }
+  }
+
+  return result;
+}
+
+console.log(task7());
 
 // Replace first space only with "-"
 
 // Replace "()” with "o"
 
 // Build a new string with only uppercase letters
-
-console.log(task2());
