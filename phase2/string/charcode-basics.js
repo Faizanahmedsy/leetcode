82d7ce7dafae6f2bcbd1b4c93198b789d36af295
FaizanahmedSy <saiyed.faizanahmed1@gmail.com>
2025-12-21 17:35:25 +0530

    feat: string

---
+++ phase2/string/charcode-basics.js
@@ -0,0 +1,37 @@
+/**
+ * TASK 1: Convert a lowercase letter to its position in the alphabet.
+ * Example: "a" -> 1, "z" -> 26
+ */
+function task1(char = "a") {}
+console.log(task1());
+
+/**
+ * TASK 2: Convert a number (1 to 26) to its lowercase alphabet letter.
+ * Example: 1 -> "a", 26 -> "z"
+ */
+function task2(num = 1) {}
+console.log(task2());
+
+/**
+ * TASK 3: Convert a string into an array of alphabet positions.
+ * Example: "abc" -> [1, 2, 3]
+ */
+function task3(str = "abc") {}
+console.log(task3());
+
+/**
+ * TASK 4: Shift each lowercase letter by 1.
+ * (like a very simple Caesar cipher)
+ * Example: "abc" -> "bcd"
+ * Assume input contains only lowercase letters.
+ */
+function task4(str = "abc") {}
+console.log(task4());
+
+/**
+ * TASK 5: Decode a string where numbers represent letters.
+ * (Same idea as LeetCode 1309 but ONLY single digits 1–9)
+ * Example: "123" -> "abc"
+ */
+function task5(str = "123") {}
+console.log(task5());
