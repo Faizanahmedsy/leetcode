e35f47b8b5648996b8dcf54b97f59d01a56eaa10
FaizanahmedSy <saiyed.faizanahmed1@gmail.com>
2025-12-28 18:02:17 +0530

    feat: hashmaps complete

--- phase2/hashmap/hashmap_prerequisites_unsolved.js
+++ phase2/hashmap/hashmap_prerequisites_unsolved.js
@@ -49,19 +49,18 @@ console.log("Task 2:", task2());
 /**
  * TASK 3: Two sum
  */
-function task3(target = 19) {
+function task3(target = 9) {
   const arr = [2, 7, 11, 15];
-  const result = [];
-  const map = new Map();
+  const seen = new Map();
 
   for (let i = 0; i < arr.length; i++) {
     const cur = arr[i];
     let remaining = target - cur;
 
-    if (map.has(remaining)) {
+    if (seen.has(remaining)) {
       return [remaining, cur];
     } else {
-      map.set(cur, remaining);
+      seen.set(cur, true);
     }
 
     // console.log(remaining, "remaining");
@@ -82,8 +81,7 @@ function task3(target = 19) {
   //     return result;
   //   }
   // }
-
-  console.log(map, "map");
+  return false;
 }
 console.log("Task 3:", task3());
 
@@ -91,7 +89,20 @@ console.log("Task 3:", task3());
  * TASK 4: Check duplicates
  */
 function task4() {
-  const arr = [1, 2, 3, 1];
+  const arr = [1, 2, 3, 5, 12, 5];
+  const map = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    const curr = arr[i];
+
+    if (map.has(curr)) {
+      return true;
+    } else {
+      map.set(curr, 1);
+    }
+  }
+
+  return false;
 }
 console.log("Task 4:", task4());
 
@@ -100,11 +111,45 @@ console.log("Task 4:", task4());
  */
 function task5() {
   const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+  const hashMap = new Map();
+  const strArr = [];
+  for (let i = 0; i < strs.length; i++) {
+    const cur = strs[i];
+    const stringKey = cur.split("").sort().join("");
+    // hashMap.set(stringKey, cur);
+
+    if (hashMap.has(stringKey)) {
+      const key = stringKey;
+      const curValue = hashMap.get(stringKey);
+      curValue.push(cur);
+      console.log("curValue", curValue);
+      // hashMap.set(stringKey, curValue.push(cur));
+    } else {
+      const key = stringKey;
+      const value = [cur];
+      hashMap.set(key, value);
+    }
+  }
+
+  // for ( let [key, value] of hashMap){
+
+  // }
+  console.log(hashMap, "hashMap");
+  return Array.from(hashMap.values());
 }
 console.log("Task 5:", task5());
 
 /**
  * TASK 6: Character frequency map
  */
-function task6(str = "hello") {}
+function task6(str = "hello") {
+  const map = new Map();
+
+  for (let i = 0; i < str.length; i++) {
+    const cur = str[i];
+    map.set(cur, (map.get(cur) || 0) + 1);
+  }
+
+  return map;
+}
 console.log("Task 6:", task6());
