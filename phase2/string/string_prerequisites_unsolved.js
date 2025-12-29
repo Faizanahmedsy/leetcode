74b65e738c9fbf702ed1dd07eb7dbe2b7c1549a6
FaizanahmedSy <saiyed.faizanahmed1@gmail.com>
2025-12-29 08:52:16 +0530

    jkasnjka

--- phase2/string/string_prerequisites_unsolved.js
+++ phase2/string/string_prerequisites_unsolved.js
@@ -19,7 +19,16 @@
  * Pattern: Build new string
  * Example: "abc" → "cba"
  */
-function task1(str = "abc") {}
+function task1(str = "abc") {
+  let newStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    const char = str[i];
+    newStr += char;
+  }
+
+  return newStr;
+}
 console.log("Task 1:", task1());
 
 /**
@@ -27,7 +36,26 @@ console.log("Task 1:", task1());
  * Pattern: Two pointers
  * Example: "madam" → true
  */
-function task2(str = "madam") {}
+function task2(str = "madam") {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    console.log("left", left);
+    console.log("right", right);
+
+    if (str[left] !== str[right]) {
+      console.log("str[left]", str[left]);
+      console.log("str[right]", str[right]);
+
+      return false;
+    }
+
+    left++;
+    right--;
+  }
+  return true;
+}
 console.log("Task 2:", task2());
 
 /**
@@ -35,7 +63,18 @@ console.log("Task 2:", task2());
  * Pattern: Accumulator
  * Example: "hello" → 2
  */
-function task3(str = "hello") {}
+function task3(str = "hello") {
+  const vowels = ["a", "e", "i", "o", "u"];
+  let numOfVowels = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (vowels.includes(char)) {
+      numOfVowels += 1;
+    }
+  }
+  return numOfVowels;
+}
 console.log("Task 3:", task3());
 
 /**
@@ -43,7 +82,18 @@ console.log("Task 3:", task3());
  * Pattern: Build new string
  * Example: "a b c" → "abc"
  */
-function task4(str = "a b c") {}
+function task4(str = "a b c") {
+  let newStr = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (char != " ") {
+      newStr += char;
+    }
+  }
+
+  return newStr;
+}
 console.log("Task 4:", task4());
 
 /**
@@ -51,7 +101,22 @@ console.log("Task 4:", task4());
  * Pattern: Frequency + traversal
  * Example: "leetcode" → "l"
  */
-function task5(str = "leetcode") {}
+function task5(str = "leetcode") {
+  const map = new Map();
+
+  for (let char of str) {
+    map.set(char, (map.get(char) || 0) + 1);
+  }
+
+  for (let char of str) {
+    if (map.get(char) === 1) {
+      return char;
+    }
+  }
+
+  return null;
+}
+
 console.log("Task 5:", task5());
 
 /**
@@ -59,5 +124,28 @@ console.log("Task 5:", task5());
  * Pattern: Frequency comparison
  * Example: "anagram", "nagaram" → true
  */
-function task6(s = "anagram", t = "nagaram") {}
+
+function task6(s = "anagram", t = "nagaram") {
+  if (s.length !== t.length) return false;
+
+  const map = new Map();
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    map.set(char, (map.get(char) || 0) + 1);
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    const char = t[i];
+
+    if (!map.get(char)) {
+      return false;
+    }
+
+    map.set(char, map.get(char) - 1);
+  }
+
+  return true;
+}
+
 console.log("Task 6:", task6());
